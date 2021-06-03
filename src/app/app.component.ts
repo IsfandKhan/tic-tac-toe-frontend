7f45d0db35f13a3be29e448dfa7698b6bf9b69a6
Isfand Yar Khan <isfandyar_khan@live.co.uk>
2021-06-03 23:36:29 +0500

    insecure proticols bipased

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tic Tac Toe';
+  constructor(private router: Router) {}
+  ngOnInit() {
+    const { protocol, hostname, pathname } = window.location;
+    const { production } = environment;
+
+    if (production && protocol === 'http:') {
+      this.router.navigateByUrl(`https://${hostname}${pathname}`);
+    }
+  }
 }
